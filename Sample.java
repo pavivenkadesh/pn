a6b82b8662eae5ee4c7db841f448f54411933724
ramuklithnes <senthil331999@gmail.com>
2018-08-06 14:36:52 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Sample
+{
+public static void main(String args[])
+{
+int x;
+Scanner sc=new Scanner(System.in)
+x=sc.nextInt();
+if(x%2==0)
+System.out.println("The number is even");
+else
+System.out.println("The number is odd");
+}
+}
